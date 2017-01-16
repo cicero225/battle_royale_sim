ba1aa320ce9dcb75ada80c7b83f1353f997f7303
Heironym <cicero225@gmail.com>
2017-01-16 00:48:43 -0800

    Whoops. Accidentally leaked the sponsor picture into "unknown sponsor.

--- Objs/Events/SponsorCheatKill.py
+++ Objs/Events/SponsorCheatKill.py
@@ -6,9 +6,11 @@ def func(Event, mainActor, state=None, participants=None, victims=None, sponsors
     mainActor.alive = False
     if random.randint(0,1):
         desc = sponsors[0].name+' cheated, killing '+mainActor.name + " while no one was looking."
+        tempList = [sponsors[0], mainActor]
     else:
         desc = 'An unknown sponsor cheated, killing '+mainActor.name + " while no one was looking."
-    return (desc, [sponsors[0], mainActor], [mainActor.name]) # Second entry is the contestants or items named in desc, in desired display. Third is anyone who died. This is in strings.
+        tempList = [mainActor]
+    return (desc, tempList, [mainActor.name]) # Second entry is the contestants or items named in desc, in desired display. Third is anyone who died. This is in strings.
 
 Event.doEventSponsorCheatKill = classmethod(func)
 
