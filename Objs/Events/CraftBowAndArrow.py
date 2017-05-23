24cbf92ca082ff6a41c114d63a5bd52cfa8d5cee
Hieronym <cicero225@gmail.com>
2017-05-22 22:45:30 -0700

    A bow and arrow set of events

---
+++ Objs/Events/CraftBowAndArrow.py
@@ -0,0 +1,16 @@
+import random
+
+from Objs.Events.Event import Event
+
+def func(self, mainActor, state=None, participants=None, victims=None, sponsors=None):
+    probSuccess = mainActor.stats["cleverness"]*0.03+mainActor.stats["survivalism"]*0.03
+    descList = [mainActor]
+    if random.random() < probSuccess:
+        mainActor.addItem(state["items"]["Bow and Arrow"])
+        descList.append(state["items"]["Bow and Arrow"])
+        desc = mainActor.name+' crafted a Bow and Arrow set, but failed.'
+    else:
+        desc = mainActor.name+' tried crafting a Bow and Arrow set, but failed.'
+    return (desc, descList, []) # Second entry is the contestants named in desc, in order. Third is anyone who died.
+
+Event.registerEvent("CraftBowAndArrow", func)
--- Objs/Events/FireArrowKill.py
+++ Objs/Events/FireArrowKill.py
@@ -2,26 +2,22 @@ from Objs.Events.Event import Event
 import random
 
 def func(self, mainActor, state=None, participants=None, victims=None, sponsors=None):
-    probKill = Event.DieOrEscapeProb1v1(mainActor, victims[0], state["settings"], defenseStat=(victims[0].stats['combat ability']*0.75+victims[0].stats['cleverness']*0.25))
-    spearBroken = random.randint(0,1)
-    tempList = [mainActor, state["items"]["Spear"], victims[0]]
+    probKill = Event.DieOrEscapeProb1v1(mainActor, victims[0], state["settings"], defenseStat=(victims[0].stats['combat ability']*0.25+victims[0].stats['cleverness']*0.15))
+    tempList = [mainActor, state["items"]["Bow and Arrow"], victims[0]]
     # Deteriorate relationship of victim toward participant
     state["allRelationships"].IncreaseFriendLevel(victims[0], mainActor, -2)
     state["allRelationships"].IncreaseLoveLevel(victims[0], mainActor, -3)
     if random.random()<probKill:
         victims[0].alive = False
         deadList = [victims[0].name]
-        desc = mainActor.name+' threw a spear through '+victims[0].name+"'s neck and killed "+Event.parseGenderObject(victims[0])+"."
+        desc = mainActor.name+' shot an arrow at '+victims[0].name+" and killed "+Event.parseGenderObject(victims[0])+"."
         lootList = Event.lootAll(mainActor, victims[0])
         if lootList:
             desc += ' '+mainActor.name+' looted the body for '+Event.englishList(lootList)+'.'
             tempList.extend(lootList)
     else:
         deadList = []
-        desc = mainActor.name+' threw a spear at '+victims[0].name+", but missed."
-    if spearBroken:
-        desc += " The spear was broken in the process."
-        mainActor.removeItem(state["items"]["Spear"])
+        desc = mainActor.name+' shot an arrow at '+victims[0].name+", but missed."
     return (desc, tempList, deadList) # Second entry is the contestants or items named in desc, in desired display. Third is anyone who died. This is in strings.
 
-Event.registerEvent("ThrowSpearKill", func)
+Event.registerEvent("FireArrowKill", func)
