9b2d4c3ecafb43f5bda63835a05926d8e7eefb0f
Hieronym <cicero225@gmail.com>
2017-05-23 20:31:44 -0700

    Typo

--- Objs/Events/CraftBowAndArrow.py
+++ Objs/Events/CraftBowAndArrow.py
@@ -8,7 +8,7 @@ def func(self, mainActor, state=None, participants=None, victims=None, sponsors=
     if random.random() < probSuccess:
         mainActor.addItem(state["items"]["Bow and Arrow"])
         descList.append(state["items"]["Bow and Arrow"])
-        desc = mainActor.name+' crafted a Bow and Arrow set, but failed.'
+        desc = mainActor.name+' crafted a Bow and Arrow set.'
     else:
         desc = mainActor.name+' tried crafting a Bow and Arrow set, but failed.'
     return (desc, descList, []) # Second entry is the contestants named in desc, in order. Third is anyone who died.
