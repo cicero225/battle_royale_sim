c68097059b83669d1998603c98ae5ebbb7194fd2
Hieronym <cicero225@gmail.com>
2017-05-25 00:00:08 -0700

    Patch to trap self-kills/modifies behavior (allows self-kills, but deals with it properly now)

--- Objs/Events/DiesFromTrap.py
+++ Objs/Events/DiesFromTrap.py
@@ -29,17 +29,20 @@ def modifyTrapChance(actor, baseEventActorWeight, event):
 Event.registerInsertedCallback("modifyIndivActorWeights", modifyTrapChance)
 
 def func(self, mainActor, state=None, participants=None, victims=None, sponsors=None):
-    # First subtract this character's own traps from the trap list
+    # First subtract this character's own traps from the trap list, weighted by chance of not being stupid
+    notBeingStupidRatio = mainActor.stats["stability"]/10
     thisTrapDict = self.eventStore["trapCounter"].copy()
     if str(mainActor) in self.eventStore["trapMakerCounter"]:
         totSum = 0
         for key in thisTrapDict:
-            thisTrapDict[key] -= self.eventStore["trapMakerCounter"][str(mainActor)][key]
+            thisTrapDict[key] -= self.eventStore["trapMakerCounter"][str(mainActor)][key]*notBeingStupidRatio 
             totSum += thisTrapDict[key]
         if not totSum:  # all existing traps were made by this character, cancel event
             return None
     chosen = weightedDictRandom(thisTrapDict, 1)[0]
     trapSourceDict = {key: self.eventStore["trapMakerCounter"][key][chosen] for key in self.eventStore["trapMakerCounter"]}
+    if str(mainActor) in trapSourceDict:
+        trapSourceDict[str(mainActor)] *= (1-notBeingStupidRatio)
     trapSource = weightedDictRandom(trapSourceDict, 1)[0]
     desc = str(mainActor) + TRAP_RESULT[chosen].replace("VICTIM", Event.parseGenderObject(mainActor)).replace("VICTRE", Event.parseGenderReflexive(mainActor)).replace("SOURCE", trapSource).replace("VICTPOS", Event.parseGenderPossessive(mainActor))
     
--- Objs/Utilities/ArenaUtils.py
+++ Objs/Utilities/ArenaUtils.py
@@ -109,10 +109,11 @@ def logKills(proceedAsUsual, eventOutputs, thisevent, mainActor, state, particip
         if str(trueKiller) not in trueKillCounterDict:
             trueKillCounterDict[str(trueKiller)] = state["callbackStore"]["killCounter"][str(trueKiller)]
         state["callbackStore"]["killCounter"][str(trueKiller)] += 1
-        state["callbackStore"]["KillThisTurnFlag"][str(trueKiller)] = True  
-        # This is treated as if someone had done the worst possible thing to the dead person. There is also a stability impact.
-        state["allRelationships"].IncreaseFriendLevel(state["contestants"][str(dead)], state["contestants"][str(trueKiller)], -10)
-        state["allRelationships"].IncreaseLoveLevel(state["contestants"][str(dead)], state["contestants"][str(trueKiller)], -10)
+        state["callbackStore"]["KillThisTurnFlag"][str(trueKiller)] = True
+        if str(trueKiller) != str(mainActor):
+            # This is treated as if someone had done the worst possible thing to the dead person. There is also a stability impact.
+            state["allRelationships"].IncreaseFriendLevel(state["contestants"][str(dead)], state["contestants"][str(trueKiller)], -10)
+            state["allRelationships"].IncreaseLoveLevel(state["contestants"][str(dead)], state["contestants"][str(trueKiller)], -10)
         state["allRelationships"].KillImpact(dead)
         
     # Modify description to reflect kills
