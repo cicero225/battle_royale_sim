77e19bd29bc4ae12742f35e231b4b2e25745a7e7
Heironym <cicero225@gmail.com>
2018-07-01 22:39:26 +0200

    Some empty skeleton files

---
+++ Objs/EventLog/EventLog.py
@@ -0,0 +1,6 @@
+# This object serves as store of changes to the internal state of the arena, allowing the in-principle
+# reacquisition of object information from any past point, though this is not intended to provide
+# the object itself or the ability to recapitulate past operation.
+
+
+# I'm unsure right now if this is really going to be useful, so unimplemented at the moment.
