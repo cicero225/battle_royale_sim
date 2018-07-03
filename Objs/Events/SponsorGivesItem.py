3f6df452aae8b303a45c38be1fca9238d48d537b
Heironym <cicero225@gmail.com>
2018-07-04 01:31:22 +0200

    Corrects an oversight in dossier friendly naming.
    Dossier stackability is now turned off.

--- Objs/Events/SponsorGivesItem.py
+++ Objs/Events/SponsorGivesItem.py
@@ -9,15 +9,17 @@ def func(self, mainActor, state=None, participants=None, victims=None, sponsors=
         return None  # Pick a different event
     gift = random.choice(options)
     mainActor.addItem(gift)
+    # To get the actual item name, we need to get the name of the item INSTANCE
+    friendlyName = mainActor.hasThing(gift)[0].friendly
     state["allRelationships"].IncreaseFriendLevel(
         mainActor, sponsors[0], random.randint(1, 2))
     choice = random.randint(0, 1)
     if choice:
         desc = sponsors[0].name + ' gave ' + \
-            gift.friendly + ' to ' + mainActor.name + "."
+            friendlyName + ' to ' + mainActor.name + "."
         return (desc, [sponsors[0], gift, mainActor], [])
     else:
-        desc = 'An unknown sponsor gave ' + gift.friendly + ' to ' + mainActor.name + "."
+        desc = 'An unknown sponsor gave ' + friendlyName + ' to ' + mainActor.name + "."
         # Second entry is the contestants or items named in desc, in desired display. Third is anyone who died. This is in strings.
         return (desc, [gift, mainActor], [])
 
