577c9816cbb21af068b796a6205ef3828f02ee94
Hieronym <cicero225@gmail.com>
2019-04-21 17:58:29 -0700

    Fixes a small text glitch on the Cornucopia fight over Items event that prevented the html from resolving Dossier details

--- Objs/Events/CornucopiaEvents/FightOverItems.py
+++ Objs/Events/CornucopiaEvents/FightOverItems.py
@@ -1,6 +1,7 @@
 from __future__ import division
 
 from Objs.Events.Event import Event
+from Objs.Items.Item import ItemInstance
 from ...Utilities.ArenaUtils import weightedDictRandom
 import collections
 import random
@@ -10,7 +11,7 @@ def func(self, mainActor, state=None, participants=None, victims=None, sponsors=
     
     eventPeople = [mainActor] + participants
     numItems = random.randint(1, 2)
-    itemsFound = random.sample(list(state['items'].values()), 2)
+    itemsFound = [ItemInstance.takeOrMakeInstance(v) for v in random.sample(list(state['items'].values()), 2)]
     descList = eventPeople + itemsFound
     Event.lootRandom(eventPeople, itemsFound)
     fightDesc, fightList, fightDead, allKillers = Event.fight(
