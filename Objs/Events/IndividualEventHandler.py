6c536831e5464eaffef5942a17a22e755f216549
Heironym <cicero225@gmail.com>
2019-08-17 19:06:35 -0700

    [UNFINISHED] Start correcting the detailed participant binding logic, and adds some notes about events that need additional work.

--- Objs/Events/IndividualEventHandler.py
+++ Objs/Events/IndividualEventHandler.py
@@ -5,12 +5,19 @@ Usage: Instantiate the object when needed, perform the necessary operations, and
 under a key such as mainActor.name which will properly identify it). When the callbacks are no longer leader, delete from the
 state. If a group of callbacks may be deleted under different circumstances, it is probably best to make two objects."""
 
+from copy import copy
 from functools import partial
+import random
 import warnings
+from Objs.Contestants.Contestant import Contestant
 from Objs.Utilities.ArenaUtils import DictToOrderedDict
 
 
 class IndividualEventHandler(object):
+    # Regulating the participants in multi-participant events stresses the event model
+    # and requires detailed tracking. Thie dict tracks who has been bound to what on global
+    # basis. Subdict is assumed to be symmetric and consistent. All contestants stored by id.
+    BOUND_PARTICIPANTS_DICT = {}  # Eventname: Actor: Other participants.
 
     def __init__(self, state):
         self.state = state
@@ -55,14 +62,27 @@ class IndividualEventHandler(object):
 
     def bindRoleForContestantAndEvent(self, roleName, fixedRoleList, relevantActor, eventName, useOtherConstestantsIfNotAvailable=False):
         anonfunc = partial(self.fixedRoleCallback, roleName,
-                           fixedRoleList, relevantActor, eventName, useOtherConstestantsIfNotAvailable)
+                           fixedRoleList, relevantActor, eventName, BOUND_PARTICIPANTS_DICT, useOtherConstestantsIfNotAvailable)
         anonfunc.eventName = eventName  # Notes on the functor for debug purposes
         anonfunc.relevantActor = relevantActor
         anonfunc.fixedRoleList = fixedRoleList
         self.registerEvent("overrideContestantEvent", anonfunc)
         
+        if not isinstance(fixedRoleList, list):
+            fixedRoleList = [fixedRoleList]
+        new_group = set(x.id for x in fixedRoleList)
+        # BOUND_PARTICIPANTS_DICT.setdefault(eventName, dict()).setdefault(relevantActor.id, set()).update(new_group)
+        new_group.update(relevantActor.id)
+        total_union = copy(new_group)
+        for x in new_group:  # depth 1 is fine if we always do this.
+            total_union.update(BOUND_PARTICIPANTS_DICT[eventName].setdefault(x, set()))
+        for x in new_group:
+            # Internally this will all be the same set.
+            BOUND_PARTICIPANTS_DICT[eventName][x] = total_union
+        
         # It must _also_ be checked that the people bound all still live. This has be done before the event is selected, to prevent the selection
         # of invalid events. Note that this is only necessary if useOtherConstestantsIfNotAvailable is False.
+        anonfunc2 = None
         if not useOtherConstestantsIfNotAvailable:
             def func(actor, origWeight, event):  # Black magic
                 if event.name == eventName and actor.name == relevantActor.name:
@@ -80,25 +100,37 @@ class IndividualEventHandler(object):
         return anonfunc, anonfunc2  # Just in case it's needed by the calling function
 
     @staticmethod
-    def fixedRoleCallback(roleName, fixedRoleList, relevantActor, eventName, useOtherConstestantsIfNotAvailable, contestantKey, thisevent, state, participants, victims, sponsorsHere):
+    def fixedRoleCallback(roleName, fixedRoleList, relevantActor, eventName, bound_participants_dict, useOtherConstestantsIfNotAvailable, contestantKey, thisevent, state, participants, victims, sponsorsHere):
         # Avoiding eval here
         roleDict = DictToOrderedDict({"participants": participants,
                                       "victims": victims,
                                       "sponsors": sponsorsHere})
-        if thisevent.name == eventName and relevantActor.name == contestantKey:
-            newRolelist = []
-            if len(fixedRoleList) < len(roleDict[roleName]):  # Not enough people to fill the roleDict
-                if not useOtherConstestantsIfNotAvailable:
-                    raise AssertionError("Not enough fixed role contestants for event: " + eventName)
-                newRolelist = random.sample(roleDict[roleName], len(roleDict[roleName]) - len(fixedRoleList))
-                # Have to clear the list BUT keep the reference
-                del roleDict[roleName][:]
-                newRolelist.extend(newRolelist)
-                newRolelist.extend(fixedRoleList)
-            else:
-                # Have to clear the list BUT keep the reference
-                del roleDict[roleName][:]
-                roleDict[roleName].extend(random.sample(fixedRoleList, len(roleDict[roleName])))
+        if thisevent.name == eventName:
+            this_participant_Dict = bound_participants_dict[eventName]
+            if relevantActor.name == contestantKey:
+                numRoles = len(roleDict[roleName])
+                liveFixedRoleList = [x for x in fixedRoleList if x.alive]
+                if len(liveFixedRoleList) < numRoles:  # Not enough people to fill the roleDict
+                    if not useOtherConstestantsIfNotAvailable:
+                        return False, True
+                    newRolelist = random.sample(roleDict[roleName], numRoles - len(liveFixedRoleList))
+                    # Have to clear the list BUT keep the reference
+                    del roleDict[roleName][:]
+                    roleDict[roleName].extend(newRolelist)
+                    roleDict[roleName].extend(liveFixedRoleList)                
+                else:
+                    # Have to clear the list BUT keep the reference
+                    del roleDict[roleName][:]
+                    roleDict[roleName].extend(random.sample(liveFixedRoleList, numRoles))
+            if roleName == "participants" :
+                # We need to parse the participants list and see if some participants must
+                # be added. In the worst case reset event.
+                for participant in participants:
+                    possible_list = this_participant_Dict.get(participant.id)
+                    if possible_list is not None:
+                        if not useOtherConstestantsIfNotAvailable:
+                            return False, True
+                            
         return True, False
 
     def banEventForSingleContestant(self, eventName, contestantName, state):
